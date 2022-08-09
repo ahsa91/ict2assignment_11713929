7a981bdf4b242b4232ca19354d52121dbc5a0f19
ahsa91 <ahsa91@gmail.com>
2022-08-09 16:57:45 +0100

    new filterMoviesCard component and story created

---
+++ src/stories/filterMoviesCardAdvanced.stories.js
@@ -0,0 +1,31 @@
+import React from "react";
+import FilterMoviesCardAdvanced from "../components/filterMoviesCardAdvanced";
+import { MemoryRouter } from "react-router";
+import { action } from "@storybook/addon-actions";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export default {
+  title: "Home Page/FilterMoviesCardAdvanced",
+  component: FilterMoviesCardAdvanced,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+    (Story) => (
+      <QueryClientProvider client={queryClient}>{Story()}</QueryClientProvider>
+    ),
+  ],
+};
+
+export const Basic = () => {
+  return <FilterMoviesCardAdvanced onUserInput={action("filter input")} />;
+};
+Basic.storyName = "Default";
